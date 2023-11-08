b13f3a1d1e9bc0bf169d69c445f2f03da2a34044
Richard <moorerichardplease@gmail.com>
2023-11-08 17:41:22 +0900

    Fix javascript so classes are correctly set to div

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //------------------------grid------------------------
-const container = document.getElementById('gridContainer');
+const container = document.querySelector('#gridContainer');
 
 for (let i = 1; i < 5; i++) {
     let row = document.createElement('div');
     row.id = `row${i}`;
-    row.class = `gridRows`;
-    document.container.appendChild(row);
+    row.setAttribute(`class`, `gridRows`);
+    container.appendChild(row);
     for (let x = 1; x <5; x++) {
         let box = document.createElement('div')
-        box.class = `rowBox`;
-        document.row.appendChild(box);
+        box.setAttribute(`class`, `rowBox`);
+        row.appendChild(box);
     }
 }
