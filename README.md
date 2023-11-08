81f6ab128042427fb327e6a082a07458bbcbc483
Richard <moorerichardplease@gmail.com>
2023-11-08 15:22:01 +0900

    Created index.html file and contents Wrote logic for 4x4 square grid in javascript Began CSS for grid divs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SketchyEtchy
+
+This project will be a culmination of my Javascript, HTML, and CSS skills at the end of the Foundation courses of The Odin Project.
---
+++ index.js
@@ -0,0 +1,14 @@
+//------------------------grid------------------------
+const container = document.getElementById('gridContainer');
+
+for (let i = 1; i < 5; i++) {
+    let row = document.createElement('div');
+    row.id = `row${i}`;
+    row.class = `gridRows`;
+    document.container.appendChild(row);
+    for (let x = 1; x <5; x++) {
+        let box = document.createElement('div')
+        box.class = `row${i}`
+        document.row.appendChild(box);
+    }
+}
